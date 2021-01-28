2ea95fc2bdf99b69b413f7abe46c1c3d32cbca92
Satya Sharma <satyasharma2964@gmail.com>
2021-01-28 21:14:22 +0530

    Links Appended

---
+++ index.js
@@ -0,0 +1,59 @@
+var delayInMilliseconds = 10000; //1 second
+
+setTimeout(function() {
+$("h2").text("to change the body background-color")
+}, delayInMilliseconds);
+
+
+
+$(".twitter").hover(function () {
+	$("#social-section").toggleClass("color-twitter")
+	$("h2").text("Twitter")
+  setTimeout()
+});
+
+$(".facebook").hover(function () {
+	$("#social-section").toggleClass("color-facebook")
+	$("h2").text("Facebook")
+  setTimeout()
+});
+
+$(".googleplus").hover(function () {
+	$("#social-section").toggleClass("color-googleplus")
+	$("h2").text("Google+")
+	setTimeout()
+
+});
+$(".github").hover(function () {
+	$("#social-section").toggleClass("color-github")
+	$("h2").text("GitHub")
+	
+	setTimeout()
+
+});
+$(".pinterest").hover(function () {
+	$("#social-section").toggleClass("color-pinterest")
+	$("h2").text("Pinterest")
+	setTimeout()
+
+});
+
+$(".dribbble").hover(function () {
+	$("#social-section").toggleClass("color-dribbble")
+	$("h2").text("Dribble")
+	setTimeout()
+
+});
+
+$(".instagram").hover(function () {
+	$("#social-section").toggleClass("color-instagram")
+	$("h2").text("Instagram")
+	setTimeout()
+
+});
+$(".linkedin").hover(function () {
+	$("#social-section").toggleClass("color-linkedin")
+	$("h2").text("linkedin")
+	setTimeout()
+
+});
